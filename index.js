7ffb0b8712f3728f6c319b36d4ae7eb61d699f31
Konstantinos Perperidis <kpax@mcbook.local>
2020-11-14 21:20:07 +0200

    added missing index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const Post = require("./models/Post");
+const { MONGODB } = require("./config.js");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({
+    req,
+    pubsub,
+  }),
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({ port: PORT });
+  })
+  .then((res) => {
+    console.log(`Server runnint at ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
